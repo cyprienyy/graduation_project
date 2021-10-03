73df54734c384614a980e5c4ba90af3a7800634f
yangyi <1194778970@qq.com>
2021-10-03 14:45:03 +0800

    2021-10-03

--- BP.py
+++ BP.py
@@ -2,7 +2,7 @@ import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
 from Data import supplys, supply_count, demands, demand_count, vehicle_capacity, distance_graph, H
-from collections import deque, Counter
+from collections import deque, Counter, defaultdict
 
 
 def compare_label1_label2(label1, label2):
@@ -21,16 +21,24 @@ def compare_label1_label2(label1, label2):
     return 0
 
 
-class label_set():
-    def __init__(self):
-        self._deque = deque()
+def compare_label1_label2_1(label1, label2):
+    return 0
 
-    def add_label(self, label):
+
+def compare_label1_label2_2(label1, label2):
+    return 0
+
+
+class label_set:
+    def __init__(self):
+        self.deque = deque()
+
+    def add_label(self, label, compare_func):
         flag = True
         _new_deque = deque()
-        while self._deque:
-            old_label = self._deque.pop()
-            res = compare_label1_label2(old_label, label)
+        while self.deque:
+            old_label = self.deque.pop()
+            res = compare_func(old_label, label)
             if res == -1:
                 flag = False
                 label.dominated = True
@@ -41,18 +49,25 @@ class label_set():
                 old_label.dominated = True
         if flag is True:
             _new_deque.append(label)
-        self._deque = _new_deque
+        self.deque = _new_deque
 
     def get_best(self):
+        if len(self._deque) == 0:
+            raise Exception('没有找到任何列')
         res = float('inf')
+        _label = None
         for _label in self._deque:
             if _label.cost < res:
                 _best = _label
                 res = _label.cost
         return _label.cost, _label.time, np.array(_label.visited[1:-1])
 
+    def print_paths(self):
+        for _label in self._deque:
+            print(_label.path)
 
-class label():
+
+class label:
     def __init__(self, nodes_cnt, place):
         self.cost = 0
         self.time = 0
@@ -83,6 +98,89 @@ class label():
             return _new_label
         else:
             return None
+
+
+class label_1():
+    def __init__(self, nodes_cnt, place):
+        self.cost = 0
+        self.time = 0
+        self.f = 0
+        self.e = 0
+        self.visited = [0] * nodes_cnt
+        self.visited[0] = 1
+        self.dominated = False
+        self.place = place
+        self.current_type = 0
+        self.count = Counter()
+        self.visited_supply = []
+        self.visited_demand = []
+        self.visited_return = []
+        self.path = [0]
+
+    def extend(self, cost_add, time_add, f_add, e_add, des, des_point, des_type):
+        _new_label = label_1(len(self.visited), des)
+
+        _new_label.current_type = des_type
+        if (self.current_type, _new_label.current_type) in ((-6, 5), (1, -6), (1, 5)):
+            return None
+
+        _new_label.visited_supply = self.visited_supply.copy()
+        _new_label.visited_demand = self.visited_demand.copy()
+        _new_label.visited_return = self.visited_return.copy()
+        _array = list()
+        if des_type == 5:
+            _array = _new_label.visited_supply
+        elif des_type == 1:
+            _array = _new_label.visited_return
+        elif des_type == -6:
+            _array = _new_label.visited_demand
+        if des_point in _array:
+            if des_point != _array[-1]:
+                return None
+        else:
+            _array.append(des_point)
+
+        _new_label.cost = self.cost + cost_add
+        _new_label.time = self.time + time_add
+        _new_label.f = self.f + f_add
+        _new_label.e = self.e + e_add
+        _new_label.visited = self.visited.copy()
+        _new_label.visited[des] = 1
+        _new_label.count = self.count.copy()
+        _new_label.path = self.path + [des]
+
+        if des_type == 5:
+            _new_label.count[des_point] += 1
+        elif des_type == 1:
+            _new_label.count[des_point] -= 1
+        if _new_label.count[des_point] >= 0 and _new_label.time <= H and _new_label.f >= 0 and _new_label.e >= 0 and (
+                _new_label.f + _new_label.e) <= vehicle_capacity and not (
+                des_type == 0 and (_new_label.f != 0 or _new_label.e != 0)):
+            return _new_label
+        else:
+            return None
+
+
+class label_graph:
+    def __init__(self):
+        self.visited = set()
+        self.paths = list()
+        self.cost = 0
+        self.time = 0
+        self.place = 0
+        self.dominated = False
+
+    def extend(self, cost_add, time_add, des_point, path, points):
+        _new_label = label_graph()
+        _new_label.cost = self.cost + cost_add
+        _new_label.time = self.time + time_add
+        _new_label.place = des_point
+        _new_label.paths = self.paths + path
+        if _new_label.time <= H and len(self.visited & points) == 0:
+            _new_label.visited = self.visited | points
+            return _new_label
+        else:
+            return
 
 
 def BP():
@@ -218,7 +316,7 @@ def BP():
                 TL.append(a)
 
             UL.append(label_0)
-            TL[0].add_label(label_0)
+            TL[0].add_label(label_0, compare_label1_label2)
 
             while UL:
                 _label = UL.pop()
@@ -244,13 +342,120 @@ def BP():
                                                            j, nodes_rel[j], 5 * f_node[j] - e_node[j])
                                 if _new_label is not None:
                                     UL.append(_new_label)
-                                    TL[j].add_label(_new_label)
+                                    TL[j].add_label(_new_label, compare_label1_label2)
 
             return TL[-1].get_best()
+
+        def label_setting_1(label_set=label_set, label=label_1):
+
+            def avoid_depot_distance_graph(_s, _t):
+                if nodes_rel[_s] == 0 or nodes_rel[_t] == 0:
+                    return 0
+                else:
+                    return distance_graph[nodes_rel[_s]][nodes_rel[_t]]
+
+            def avoid_depot_x_ij(_s, _t):
+                if nodes_rel[_s] == 0 or nodes_rel[_t] == 0:
+                    return x_ij[i, j].obj - distance_graph[nodes_rel[_s]][nodes_rel[_t]]
+                else:
+                    return x_ij[i, j].obj
+
+            # 成本，时间，满电电池数，缺电电池数，拜访情况列表, dominates
+            label_0 = label(nodes_cnt, 0)
+            UL = deque()
+            TL = list()
+            for _ in range(nodes_cnt):
+                a = label_set()
+                TL.append(a)
+
+            UL.append(label_0)
+            TL[0].add_label(label_0, compare_label1_label2_1)
+
+            while UL:
+                _label = UL.pop()
+                if _label.dominated is False and _label.place != count:
+                    # print('开始拓展')
+
+                    if _label.f == 0 and _label.e == 0 and _label.place != 0:
+                        i = _label.place
+                        j = nodes_cnt - 1
+                        _new_label = _label.extend(avoid_depot_x_ij(i, j), avoid_depot_distance_graph(i, j),
+                                                   f_node[j], e_node[j], j, nodes_rel[j], 5 * f_node[j] - e_node[j])
+                        if _new_label is not None:
+                            UL.append(_new_label)
+                            TL[j].add_label(_new_label, compare_label1_label2_1)
+                        continue
+
+                    i = _label.place
+
+                    _prev_extend_point = None
+                    _prev_extend_point_type = None
+
+                    for j, to_visit in enumerate(_label.visited):
+                        if to_visit == 0 and (nodes_rel[j] != _prev_extend_point or (
+                                5 * f_node[j] - e_node[j]) != _prev_extend_point_type):
+                            _prev_extend_point = nodes_rel[j]
+                            _prev_extend_point_type = 5 * f_node[j] - e_node[j]
+                            if i != count and j != 0 and i != j and not (
+                                    i == 0 and j in demand_nodes + return_nodes) and not (
+                                    1 <= i <= (supply_num + demand_num) and j == count) and not (
+                                    count > i > (supply_num + demand_num) and return_supply[i] == j) or (
+                                    count > j > (supply_num + demand_num) and return_supply[j] == i):
+                                _new_label = _label.extend(avoid_depot_x_ij(i, j), avoid_depot_distance_graph(i, j),
+                                                           f_node[j], e_node[j],
+                                                           j, nodes_rel[j], 5 * f_node[j] - e_node[j])
+                                if _new_label is not None:
+                                    UL.append(_new_label)
+                                    TL[j].add_label(_new_label, compare_label1_label2_1)
+
+            return TL[-1].deque
+            # 注意此时的路径长度是算入了depot距离的，所以后续要进行处理
+
+        def label_setting_graph(sub_routes_obtained):
+            label_0 = label_graph()
+            UL = deque()
+            TL = dict()
+            for j in supplys + [0, nodes_cnt - 1]:
+                a = label_set()
+                TL[j] = a
+            UL.append(label_0)
+            TL[0].add_label(label_0, compare_label1_label2_2)
+
+            sub_routes_set = defaultdict(list)
+            for j in [0] + supplys:
+                for k in supplys:
+                    if j != k:
+                        sub_routes_set[j].append((distance_graph[j][k], distance_graph[j][k], k, [j, k], set()))
+            for k in supplys:
+                sub_routes_set[k].append((distance_graph[k][0], distance_graph[k][0], nodes_cnt - 1, [k, 0], set()))
+
+            for _label in sub_routes_obtained:
+                if len(_label.path) > 2:
+                    sub_routes_set[nodes_rel[_label.path[1]]].append(
+                        (_label.cost, _label.time, nodes_rel[_label.path[-2]], _label.path[1:-1],
+                         set(_label.path[1:-1])))
+
+            while UL:
+                _label = UL.pop()
+                if _label.dominated is False and _label.place != count:
+                    # print('开始拓展')
+                    i = _label.place
+                    for _sub_route in sub_routes_set[i]:
+                        _new_label = _label.extend(_sub_route[0], _sub_route[1], _sub_route[2], _sub_route[3],
+                                                   _sub_route[4])
+                        if _new_label is not None:
+                            UL.append(_new_label)
+                            TL[_new_label.place].add_label(_new_label, compare_label1_label2_2)
+
+            return TL[count].get_best()
 
         def return_val_and_coeff():
             return 0, 0
 
+        _tmp = label_setting_1()
+        _ans = label_setting_graph(_tmp)
+
+        '''
         column_cost, column_val, columnCoeff = label_setting()
         columnCoeff = columnCoeff[:demand_num + supply_num]
 
@@ -279,7 +484,7 @@ def BP():
         for v in MainProbRelax.getVars():
             if v.X != 0.0:
                 print('%s %g' % (v.VarName, v.X))
-
+        '''
     except gp.GurobiError as e:
         print('Error code ' + str(e.errno) + ": " + str(e))
 
--- Test.py
+++
@@ -1,4 +0,0 @@
-from collections import Counter
-
-a = Counter([5,5,3])
-print(sum(a.values()))
